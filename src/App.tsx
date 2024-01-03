978b4b50af3484cf7da974c3b176f03c0a34b42c
BlackCat <neko.sword@gmail.com>
2024-01-03 23:39:27 +0700

    fix: error for building

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/test1" element={<Test1 />} />
-        <Route path="/test3" element={<Test3 />} />
+        <Route path="/react-test/test1" element={<Test1 />} />
+        <Route path="/react-test/test3" element={<Test3 />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </Router>
